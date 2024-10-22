67f3375a511c0ae5da518c165c66a7a9b9d76bca
Sefa Erdoğdugil <118279009+sefaerdogdugil@users.noreply.github.com>
2024-10-22 16:51:52 +0300

    Update odevPy.py
    Update

--- odevPy.py
+++ odevPy.py
@@ -2,27 +2,27 @@ def dikdortgen_cizdir(genislik, yukseklik):
     for i in range(yukseklik):
         print('*' * genislik)
 
-
 def paralel_kenar_cizdir(kenar_uzunlugu, yukseklik):
     for i in range(yukseklik):
         print(' ' * i + '*' * kenar_uzunlugu)
-
 
 def ikizkenar_dik_ucgen_cizdir(kenar_uzunlugu):
     for i in range(1, kenar_uzunlugu + 1):
         print('*' * i)
 
-
-
-
-def main():
-    print("(1) Dikdörgen Çizdirme")
+while True:
+    print("\n(1) Dikdörtgen Çizdirme")
     print("(2) Paralel Kenar Çizdirme")
     print("(3) İkizkenar Dik Üçgen Çizdirme")
-
+    print("Çıkmak için -1'e basınız.")
+    
     secim = int(input("Seçiminizi Giriniz (1/2/3): "))
+    
+    if secim == -1:
+        print("Çıkılıyor...")
+        break
 
-    if secim == 1:
+    elif secim == 1:
         genislik = int(input("Dikdörtgenin genişliğini giriniz: "))
         yukseklik = int(input("Dikdörtgenin yüksekliğini giriniz: "))
         dikdortgen_cizdir(genislik, yukseklik)
@@ -33,12 +33,8 @@ def main():
         paralel_kenar_cizdir(kenar_uzunlugu, yukseklik)
 
     elif secim == 3:
-        kenar_uzunlugu = int(input("İkizkenar dik üçgenin ikiz kenarının uzunluğunu giriniz: "))
+        kenar_uzunlugu = int(input("İkizkenar dik üçgenin kenar uzunluğunu giriniz: "))
         ikizkenar_dik_ucgen_cizdir(kenar_uzunlugu)
 
     else:
-        print("Geçersiz seçim! Lütfen 1, 2 veya 3 giriniz.")
-
-
-if __name__ == "__main__":
-    main()
+        print("Yanlış değer girdiniz, lütfen tekrar deneyiniz.")
